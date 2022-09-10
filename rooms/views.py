0aa4aba5cfbb09170e2595acaea265006d1ae028
LeoKnox <LeoKnox@users.noreply.github.com>
2022-09-09 23:15:23 -0700

    Add files via upload

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Room
+from rest_framework import viewsets
+from .serializers import RoomSerializer
 
-# Create your views here.
+class RoomsViewset(viewsets.ModelViewSet):
+    serializer = RoomSerializer
+    queryset = Room.objects.all()
