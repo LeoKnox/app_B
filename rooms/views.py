52d1c9fd7fdf11ec373b329aa594f8c9f7c921c4
LeoKnox <LeoKnox@users.noreply.github.com>
2022-09-10 23:05:28 -0700

    Add files via upload

--- rooms/views.py
+++ rooms/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Room
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from .serializers import RoomSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
@@ -18,3 +18,14 @@ def ApiOverview(request):
         'Delete': '/item/pk/delete'
     }
     return Response(api_urls)
+
+@api_view(['POST'])
+def add_room(request):
+    room = RoomSerializer(data=request.data)
+    if Room.objects.filter(**request.data).exists():
+        raise serializers.ValidationError('Duplicate data')
+    if room.is_valid():
+        room.save()
+        return Response(room.data)
+    else:
+        return Response(status = status.HTTP_404_NOT_FOUND)
