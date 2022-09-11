761b2a80e039acdd1bb891e9b05351faad225330
LeoKnox <LeoKnox@users.noreply.github.com>
2022-09-10 22:36:48 -0700

    Add files via upload

--- rooms/views.py
+++ rooms/views.py
@@ -2,7 +2,15 @@ from django.shortcuts import render
 from .models import Room
 from rest_framework import viewsets
 from .serializers import RoomSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 class RoomsViewset(viewsets.ModelViewSet):
-    serializer = RoomSerializer
+    serializer_class = RoomSerializer
     queryset = Room.objects.all()
+
+@api_view(['GET', 'POST'])
+def create_room(request):
+    if request.method == 'POST':
+        return Response({"message": "data posted"})
+    return Response({"message": "data get"})
